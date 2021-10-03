b520b85ef60feac9205495067eae61b115fe70ab
Joseph Armas <josarmas9416@hotmail.com>
2021-10-02 21:26:42 -0500

    [FIX] universal_discount: Fixed conflicts with electronic documents

---
+++ universal_discount/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Universal Discount",
+    "summary": """
+        Universal Discount v14.0""",
+    "author": "Ksolves India Pvt. Ltd.",
+    "website": "https://github.com/waalo-fenixdoo/open-modules",
+    "images": ["static/description/Universal-Discount-V13.jpg"],
+    "category": "Sales Management",
+    "version": "14.0.1.1.0",
+    "license": "LGPL-3",
+    "depends": ["base", "sale", "purchase", "sale_management"],
+    "data": [
+        "views/ks_sale_order.xml",
+        "views/ks_account_invoice.xml",
+        "views/ks_purchase_order.xml",
+        "views/ks_account_invoice_supplier_form.xml",
+        "views/ks_account_account.xml",
+        "views/ks_report.xml",
+        "views/assets.xml",
+    ],
+}
---
+++ universal_discount/readme/DESCRIPTION.rst
@@ -0,0 +1,11 @@
+This module extends the functionality of sale and purchase
+and to allow you to:
+Apply a field in Sales, Purchase and Invoice module to calculate
+discount after the order lines are inserted.
+Discount values can be given in two types:
+* In Percent
+* In Amount
+
+Maintains the global discount entries in accounts specified by you.
+Label given to you will be used as name given to discount field.
+Also update the report PDF printout with global discount value.
